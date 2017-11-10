a5e5341b31ad819bb47677b9f5f0611d51a7bd4f
April Rivera <aprilcr87@gmail.com>
2017-11-10 00:54:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray('chocolateBars', Milky way){
-  return chocolateBars=['Milky way',... chocolateBars];
+function addElementToBeginningOfArray(array, element){
+  return ['Milky Way',...chocolateBars];
 }
 addElementToBeginningOfArray();
